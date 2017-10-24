a7f930d20b06d407e67180776b188986567ba00e
Patrick Rechnitzer <you@example.com>
2017-10-24 12:57:55 +0200

    added Oauth functionality for connection to Xena

--- index.js
+++ index.js
@@ -9,3 +9,71 @@ app.get("/", function (req, resp) {
 app.listen(app.get('port'), function () {
     console.log("Server is running on port ", app.get('port'));
 });
+var BodyParser = require("body-parser");
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+var querystring = require("query-string");
+var request = require("request");
+//import * as cookieParser from 'cookie-parser';
+//app.use (Express.static(__dirname + "/public")).use(cookieParser());
+var generateRandomString = function (length) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+var myClient_id = "4b2dab3b-0bf0-4a0e-b253-d1c102da3210.apps.xena.biz";
+var myClient_secret = "JjFEh3aanXYrvAi6ZyuIOn7s";
+var myRedirect_uri = "https://hidden-brook-94877/callback";
+var stateKey = "spotify<<_auth_state";
+app.get("/login", function (req, resp) {
+    var myScope = "api";
+    var myState = generateRandomString(16);
+    resp.cookie(stateKey, myState);
+    resp.redirect("https://login.xena.biz/connect/authorize?" +
+        querystring.stringify({
+            response_type: "code",
+            client_id: myClient_id,
+            scope: myScope,
+            redirect_uri: myRedirect_uri,
+            state: myState
+        }));
+});
+app.get("/callback", function (req, resp) {
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+    var storedState = req.cookies ? req.cookies[stateKey] : null;
+    if (state === null || state !== storedState) {
+        resp.redirect('/#' +
+            querystring.stringify({
+                error: 'state_mismatch'
+            }));
+    }
+    else {
+        resp.clearCookie(stateKey);
+        var authOptions = {
+            url: 'https://login.xena.biz/connect/authorize?',
+            form: {
+                code: code,
+                redirect_uri: myRedirect_uri,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (new Buffer(myClient_id + ':' + myClient_secret).toString('base64'))
+            },
+            json: true
+        };
+        request.post(authOptions, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var access_token = body.access_token, refresh_token = body.refresh_token;
+                var options = {
+                    url: 'https://login.xena.biz/connect/authorize?',
+                    headers: { 'Authorization': 'Bearer ' + access_token },
+                    json: true
+                };
+            }
+        });
+    }
+});
--- index.ts
+++ index.ts
@@ -10,3 +10,90 @@ app.get("/", (req, resp) => {
 app.listen(app.get('port'), () => {
     console.log("Server is running on port ", app.get('port'));
 });
+
+import * as BodyParser from 'body-parser';
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true}));
+
+import * as querystring from 'query-string';
+import * as request from 'request';
+//import * as cookieParser from 'cookie-parser';
+
+//app.use (Express.static(__dirname + "/public")).use(cookieParser());
+
+let generateRandomString = function (length: number) {
+    let text = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+
+let myClient_id = "4b2dab3b-0bf0-4a0e-b253-d1c102da3210.apps.xena.biz";
+let myClient_secret = "JjFEh3aanXYrvAi6ZyuIOn7s";
+let myRedirect_uri = "https://hidden-brook-94877/callback"
+let stateKey = "spotify<<_auth_state";
+
+app.get("/login", (req, resp) => {
+    let myScope = "api";
+    let myState = generateRandomString(16);
+
+    resp.cookie(stateKey, myState);
+
+
+    resp.redirect("https://login.xena.biz/connect/authorize?" +
+    querystring.stringify({
+        response_type: "code",
+        client_id: myClient_id,
+        scope: myScope,
+        redirect_uri: myRedirect_uri,
+        state: myState
+    }));
+});
+
+app.get("/callback", (req, resp) => {
+    let code = req.query.code || null;
+    let state = req.query.state || null;
+    let storedState = req.cookies ? req.cookies[stateKey] : null;
+
+    if (state === null || state !== storedState) {
+        resp.redirect('/#' +
+            querystring.stringify({
+                error: 'state_mismatch'
+            }));
+    } else {
+        resp.clearCookie(stateKey);
+        let authOptions = {
+            url: 'https://login.xena.biz/connect/authorize?',
+            form: {
+                code: code,
+                redirect_uri: myRedirect_uri,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (new Buffer(myClient_id + ':' + myClient_secret).toString('base64'))
+            },
+            json: true
+        };
+
+        request.post(authOptions, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+
+                let access_token = body.access_token,
+                    refresh_token = body.refresh_token;
+
+                let options = {
+                    url: 'https://login.xena.biz/connect/authorize?',
+                    headers: { 'Authorization': 'Bearer ' + access_token },
+                    json: true
+                };
+            }
+        });
+    }
+
+});
+
+
+
