9ced180f2c9e8a23dfd5f57a8a2044a7c732b321
TroelsHJ <jensen_troels@hotmail.com>
2017-10-26 10:40:34 +0200

    tried added grant_types to /login

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var request = require("request");
 var FileHandler = require("fs");
 var secretKey = FileHandler.readFileSync("./secretKey.txt");
 //#endregion
-//#region app....
+//#region endpoints
 app.set('port', (process.env.PORT || 5000));
 app.get("/", function (req, resp) {
     resp.sendFile(__dirname + "/index.html");
@@ -29,41 +29,39 @@ var generateRandomString = function (length) {
     return text;
 };
 var myClient_id = "4b2dab3b-0bf0-4a0e-b253-d1c102da3210.apps.xena.biz";
-var myClient_secret = secretKey;
-var myRedirect_uri = "https://hidden-brook-94877.herokuapps.com";
-//let stateKey = "spotify<<_auth_state";
+var myClient_secret = "JjFEh3aanXYrvAi6ZyuIOn7s";
+var myRedirect_uri = "https://hidden-brook-94877.herokuapps.com/callback";
 app.get("/login", function (req, resp) {
     var myScope = "openid testapi";
-    var myNonce = "" + generateRandomString(32);
-    //let myState = generateRandomString(16);
-    //resp.cookie(stateKey, myState);
+    var myNonce = generateRandomString(32);
     resp.redirect("https://login.xena.biz/connect/authorize?" +
         querystring.stringify({
             response_type: "code id_token",
             client_id: myClient_id,
             redirect_uri: myRedirect_uri,
             respons_mode: "form_post",
+            grant_type: 'authorization_code',
             scope: myScope,
-            nonce: myNonce,
+            nonce: myNonce
         }));
 });
-app.get("/callback", function (req, resp) {
-    var code = req.query.code || null;
-    var state = req.query.state || null;
-    var storedState = req.cookies ? req.cookies[stateKey] : null;
+/*app.get("/callback", (req, resp) => {
+    let code = req.query.code || null;
+    let state = req.query.state || null;
+    let storedState = req.cookies ? req.cookies[stateKey] : null;
+
     if (state === null || state !== storedState) {
         resp.redirect('/#' +
             querystring.stringify({
                 error: 'state_mismatch'
             }));
-    }
-    else {
+    } else {
         resp.clearCookie(stateKey);
-        var authOptions = {
+        let authOptions = {
             url: 'https://login.xena.biz/connect/authorize?',
             form: {
                 code: code,
-                redirect_uri: myRedirect_uri,
+                redirect_uri: myRedirect_uri
                 grant_type: 'authorization_code'
             },
             headers: {
@@ -71,10 +69,14 @@ app.get("/callback", function (req, resp) {
             },
             json: true
         };
+
         request.post(authOptions, function (error, response, body) {
             if (!error && response.statusCode === 200) {
-                var access_token = body.access_token, refresh_token = body.refresh_token;
-                var options = {
+
+                let access_token = body.access_token,
+                    refresh_token = body.refresh_token;
+
+                let options = {
                     url: 'https://login.xena.biz/connect/authorize?',
                     headers: { 'Authorization': 'Bearer ' + access_token },
                     json: true
@@ -82,4 +84,44 @@ app.get("/callback", function (req, resp) {
             }
         });
     }
+
+});*/
+app.get("/callback", function (req, res) {
+    // your application requests refresh and access tokens
+    // after checking the state parameter
+    var code = req.query.code || null;
+    var authOptions = {
+        url: 'https://login.xena.biz/connect/token?',
+        form: {
+            code: code,
+            redirect_uri: myRedirect_uri,
+            grant_type: 'authorization_code'
+        },
+        headers: {
+            'Authorization': 'Basic ' + (new Buffer(myClient_id + ':' + myClient_secret).toString('base64'))
+        },
+        json: true
+    };
+    request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var access_token = body.access_token, refresh_token = body.refresh_token;
+            var options = {
+                url: 'https://my.xena.biz/',
+                headers: { 'Authorization': 'Bearer ' + access_token },
+                json: true
+            };
+            // we can also pass the token to the browser to make requests from there
+            res.redirect('/#' +
+                querystring.stringify({
+                    access_token: access_token,
+                    refresh_token: refresh_token
+                }));
+        }
+        else {
+            res.redirect('/#' +
+                querystring.stringify({
+                    error: 'invalid_token'
+                }));
+        }
+    });
 });
--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import * as Express from 'express';
 let app = Express();
 import * as BodyParser from 'body-parser';
 app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({ extended: true}));
+app.use(BodyParser.urlencoded({ extended: true }));
 
 import * as querystring from 'query-string';
 import * as request from 'request';
@@ -11,7 +11,7 @@ import * as FileHandler from 'fs';
 let secretKey = FileHandler.readFileSync("./secretKey.txt");
 //#endregion
 
-//#region app....
+//#region endpoints
 app.set('port', (process.env.PORT || 5000));
 
 app.get("/", (req, resp) => {
@@ -34,30 +34,26 @@ let generateRandomString = function (length: number) {
 };
 
 let myClient_id = "4b2dab3b-0bf0-4a0e-b253-d1c102da3210.apps.xena.biz";
-let myClient_secret = secretKey;
-let myRedirect_uri = "https://hidden-brook-94877.herokuapps.com"
-//let stateKey = "spotify<<_auth_state";
+let myClient_secret = "JjFEh3aanXYrvAi6ZyuIOn7s";
+let myRedirect_uri = "https://hidden-brook-94877.herokuapps.com/callback"
 
 app.get("/login", (req, resp) => {
     let myScope = "openid testapi";
-    let myNonce = "" + generateRandomString(32);
-    //let myState = generateRandomString(16);
-
-    //resp.cookie(stateKey, myState);
-
+    let myNonce = generateRandomString(32);
 
     resp.redirect("https://login.xena.biz/connect/authorize?" +
-    querystring.stringify({
-        response_type: "code id_token",
-        client_id: myClient_id,
-        redirect_uri: myRedirect_uri,
-        respons_mode: "form_post",
-        scope: myScope,
-        nonce: myNonce,
-    }));
+        querystring.stringify({
+            response_type: "code id_token",
+            client_id: myClient_id,
+            redirect_uri: myRedirect_uri,
+            respons_mode: "form_post",
+            grant_type: 'authorization_code',
+            scope: myScope,
+            nonce: myNonce
+        }));
 });
 
-app.get("/callback", (req, resp) => {
+/*app.get("/callback", (req, resp) => {
     let code = req.query.code || null;
     let state = req.query.state || null;
     let storedState = req.cookies ? req.cookies[stateKey] : null;
@@ -96,6 +92,54 @@ app.get("/callback", (req, resp) => {
             }
         });
     }
+
+});*/
+
+app.get("/callback", function (req, res) {
+
+    // your application requests refresh and access tokens
+    // after checking the state parameter
+
+    let code = req.query.code || null;
+
+    let authOptions = {
+        url: 'https://login.xena.biz/connect/token?',
+        form: {
+            code: code,
+            redirect_uri: myRedirect_uri,
+            grant_type: 'authorization_code'
+        },
+        headers: {
+            'Authorization': 'Basic ' + (new Buffer(myClient_id + ':' + myClient_secret).toString('base64'))
+        },
+        json: true
+    };
+
+    request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+
+            let access_token = body.access_token,
+                refresh_token = body.refresh_token;
+
+            let options = {
+                url: 'https://my.xena.biz/',
+                headers: { 'Authorization': 'Bearer ' + access_token },
+                json: true
+            };
+
+            // we can also pass the token to the browser to make requests from there
+            res.redirect('/#' +
+                querystring.stringify({
+                    access_token: access_token,
+                    refresh_token: refresh_token
+                }));
+        } else {
+            res.redirect('/#' +
+                querystring.stringify({
+                    error: 'invalid_token'
+                }));
+        }
+    });
 
 });
 
